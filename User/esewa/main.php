e9c97b43442533d845a6339aaaa64e22ffaf9b52
Prama123-ui <panditprama@gmail.com>
2022-01-20 16:24:32 +0545

    PHP Project

--- User/esewa/main.php
+++ User/esewa/main.php
@@ -1,3 +1,8 @@
+<?php
+include 'setting.php';
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -10,16 +15,16 @@
 </head>
 <body>
 
-    <form action="https://uat.esewa.com.np/epay/main" method="POST">
+    <form action="<?php echo $epay_url?>" method="POST">
     <input value="100" name="tAmt" type="hidden">
-    <input value="90" name="amt" type="hidden">
-    <input value="5" name="txAmt" type="hidden">
-    <input value="2" name="psc" type="hidden">
-    <input value="3" name="pdc" type="hidden">
+    <input value="100" name="amt" type="hidden">
+    <input value="0" name="txAmt" type="hidden">
+    <input value="0" name="psc" type="hidden">
+    <input value="0" name="pdc" type="hidden">
     <input value="EPAYTEST" name="scd" type="hidden">
-    <input value="ee2c3ca1-696b-4cc5-a6be-2c40d929d453" name="pid" type="hidden">
+    <input value="<?php echo $pid?>" name="pid" type="hidden">
     <input value="http://merchant.com.np/page/esewa_payment_success?q=su" type="hidden" name="su">
-    <input value="http://merchant.com.np/page/esewa_payment_failed?q=fu" type="hidden" name="fu">
+    <input value="http://localhost/FoodOrderingSystem/Online-Food-Ordering-System-With-PHP/User/esewa/failed.php" type="hidden" name="fu">
     <br><input value="Esewa Payment" class="btn btn-primary" type="submit" >
     </form>
 
